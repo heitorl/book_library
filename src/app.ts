c0fb6c20b5884c500d144bbe0821bf4639414a62
heitor lourenco <juniorio01@hotmail.com>
2023-09-20 20:01:47 -0300

    change import cors to require

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import Routers from "./router";
-import cors from "cors";
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 Routers(app);
 
